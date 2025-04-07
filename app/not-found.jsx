0650fe8e3338af1b6f126d4aea1cede62d624395
sahil kapase <sahilkapase97@gmal.com>
2025-04-08 01:07:31 +0530

    first commit

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[100vh] px-4 text-center">
-      <h1 className="text-6xl font-bold gradient-title mb-4">404</h1>
-      <h2 className="text-2xl font-semibold mb-4">Page Not Found</h2>
+      <h1 className="text-6xl font-bold gradient-title mb-4">
+        <Image
+          src="/page-not-found.png"
+          alt="Description of image"
+          width={800}
+          height={800}
+        />
+      </h1>
+
       <p className="text-gray-600 mb-8">
         Oops! The page you&apos;re looking for doesn&apos;t exist or has been
         moved.
--- components/hero.jsx
+++ components/hero.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="/transaction/create">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
